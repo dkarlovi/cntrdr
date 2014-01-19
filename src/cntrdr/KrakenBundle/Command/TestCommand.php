73be59092d2ff8008268819dec203964fb0819c1
Dalibor Karlović <dkarlovi@gmail.com>
2014-01-19 13:05:20 +0100

    Kraken client integration, initial

---
+++ src/cntrdr/KrakenBundle/Command/TestCommand.php
@@ -0,0 +1,26 @@
+<?php
+namespace cntrdr\KrakenBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TestCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('kraken:test')
+            ->setDescription('Test Kraken API');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $krakenClient = $this->getContainer()->get('krakenClient');
+        
+        $res = $krakenClient->QueryPublic('Assets');
+        print_r($res);
+        
+        $output->writeln('Yes!');
+    }
+}
